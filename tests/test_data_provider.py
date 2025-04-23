3abe664da75001f3e9dcacb0c5144e2a3d9a2a90
Jonathan Leo <jonathan.leo@answerrocket.com>
2025-04-23 14:02:04 -0400

    checking if the expected columns are coming back from overproof's pull_data rather than if it ran. Adding test cases for sales uplift with other metrics and breaking out by non-cross breakout dimensions, like brand_name.

--- tests/test_data_provider.py
+++ tests/test_data_provider.py
@@ -6,10 +6,11 @@ class TestPullData:
 
     metric__sales_uplift = MenuColNames.SALES_UPLIFT_METRIC.value
     metric__sold_9le = MenuColNames.SOLD_9LE_METRIC.value
-
+    metric__sold_cases = MenuColNames.SOLD_CASES_METRIC.value
+    
     breakout__cocktail_group = MenuColNames.COCKTAIL_GROUP_COL.value
     breakout__max_time_month = MenuColNames.MAX_TIME_MONTH_COL.value
-    # breakout2 = "base_size"
+    breakout__brand_name = MenuColNames.BRAND_NAME_COL.value
 
     filter__max_time_date__2024 = {'col': MenuColNames.MAX_TIME_DATE_COL.value, 'op': 'BETWEEN', 'val': "'2024-01-01' AND '2024-03-31'"}
     filter__brand_name__papas_pilar = {"col": MenuColNames.BRAND_NAME_COL.value, "op": "=", "val": "papa's pilar"}
@@ -23,6 +24,12 @@ class TestPullData:
         metric_props = tpms.get_metric_props()
 
         return [helper.get_metric_prop(metric, metric_props) for metric in metrics]
+    
+    def assert_value_between(self, value, min_value, max_value):
+        assert min_value <= value <= max_value, f"{value} is not between {min_value} and {max_value}"
+    
+    def assert_value_between_threshold(self, value, expected_value, threshold):
+        self.assert_value_between(value, expected_value - threshold, expected_value + threshold)
 
     def test_sales_uplift_by_cocktail_group_in_2024(self):
         df = self.pull_data_function(
@@ -30,7 +37,8 @@ class TestPullData:
             breakouts = [self.breakout__cocktail_group],
             filters = [self.filter__max_time_date__2024]
         )
-        assert True
+        assert self.metric__sales_uplift in df.columns
+        assert self.breakout__cocktail_group in df.columns
 
     def test_sold_9le_by_cocktail_group_in_2024(self):
         df = self.pull_data_function(
@@ -38,7 +46,8 @@ class TestPullData:
             breakouts = [self.breakout__cocktail_group],
             filters = [self.filter__max_time_date__2024]
         )
-        assert True
+        assert self.metric__sold_9le in df.columns
+        assert self.breakout__cocktail_group in df.columns
  
     def test_sales_uplift_by_cocktail_group_filtered_to_papas_pilar_in_2024(self):
         df = self.pull_data_function(
@@ -46,7 +55,8 @@ class TestPullData:
             breakouts = [self.breakout__cocktail_group],
             filters = [self.filter__brand_name__papas_pilar, self.filter__max_time_date__2024]
         )
-        assert True
+        assert self.metric__sales_uplift in df.columns
+        assert self.breakout__cocktail_group in df.columns
         
     def test_sold_9le_by_cocktail_group_filtered_to_papas_pilar_in_2024(self):
         df = self.pull_data_function(
@@ -54,7 +64,8 @@ class TestPullData:
             breakouts = [self.breakout__cocktail_group],
             filters = [self.filter__brand_name__papas_pilar, self.filter__max_time_date__2024]
         )
-        assert True
+        assert self.metric__sold_9le in df.columns
+        assert self.breakout__cocktail_group in df.columns
 
     def test_sales_uplift_filtered_to_papas_pilar_and_daiquiri_in_2024(self):
         df = self.pull_data_function(
@@ -65,7 +76,12 @@ class TestPullData:
                 self.filter__max_time_date__2024
             ]
         )
-        assert True
+        assert self.metric__sales_uplift in df.columns
+        assert len(df) == 1
+
+        # TODO: Validate this value
+        uplift_value = df[self.metric__sales_uplift].values[0]
+        self.assert_value_between_threshold(uplift_value, 0.26769106669107506, 0.001)
 
     def test_sales_uplift_by_month_in_2024_filtered_to_papas_pilar_and_daiquiri(self):
         df = self.pull_data_function(
@@ -77,4 +93,30 @@ class TestPullData:
                 self.filter__max_time_date__2024
             ]
         )
-        assert True
+        assert self.metric__sales_uplift in df.columns
+        assert self.breakout__max_time_month in df.columns
+
+    def test_sales_uplift_and_sold_cases_in_q1_2024(self):
+
+        df = self.pull_data_function(
+            metrics = self._get_metrics([self.metric__sales_uplift, self.metric__sold_cases]),
+            filters = [self.filter__brand_name__papas_pilar, self.filter__max_time_date__2024]
+        )
+        assert self.metric__sales_uplift in df.columns
+        assert self.metric__sold_cases in df.columns
+
+    def test_sales_uplift_by_brand_filtered_to_daiquiri_in_q1_2024(self):
+        """
+        Testing a non-cross breakout.
+        """
+
+        df = self.pull_data_function(
+            metrics = self._get_metrics([self.metric__sales_uplift]),
+            breakouts = [self.breakout__brand_name],
+            filters = [
+                self.filter__ingredient_of_cocktail_group__daiquiri, 
+                self.filter__max_time_date__2024
+            ]
+        )
+        assert self.breakout__brand_name in df.columns
+        assert self.metric__sales_uplift in df.columns
