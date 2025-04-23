e92c7e8d3c0ac218a24625ad08b41e2c7ef63ad8
Jonathan Leo <jonathan.leo@answerrocket.com>
2025-04-23 09:14:29 -0400

    [CON-3787] Uplift metric in memory calculation.

--- overproof_data_provider.py
+++ overproof_data_provider.py
@@ -1,4 +1,6 @@
-from ar_analytics.helpers.utils import pull_data
+from typing import Optional
+from ar_analytics.helpers.utils import pull_data, exit_with_status
+from overproof_utilities import MenuColNames
 import pandas as pd
 import numpy as np
 
@@ -11,16 +13,30 @@ class DataProvider(object):
         self.menu_dataset = "326b5bd5-55bd-49bd-a2e5-f601b1c25d52" # Snowflake Ar Max Menu Table
         self.depletion_dataset = "88a8a548-b4bc-4800-8397-e7d7f8d0bdb4" # Snowflake Ar Max Depletions
 
-        self.cross_dims = ["cocktail__name", "cocktail__style", "cocktail_family", "cocktail_group", "cocktail__flavors", "cocktail__derived_from", "menu_item__item_name", "menu_item__item_type"]
+        self.cross_dims = [
+            MenuColNames.COCKTAIL_NAME_COL.value, 
+            MenuColNames.COCKTAIL_STYLE_COL.value, 
+            MenuColNames.COCKTAIL_FAMILY_COL.value, 
+            MenuColNames.COCKTAIL_GROUP_COL.value, 
+            MenuColNames.COCKTAIL_FLAVORS_COL.value, 
+            MenuColNames.COCKTAIL_DERIVED_FROM_COL.value, 
+            MenuColNames.INGREDIENT_OF_COCKTAIL_NAME_COL.value,
+            MenuColNames.INGREDIENT_OF_COCKTAIL_GROUP_COL.value,
+            MenuColNames.INGREDIENT_OF_COCKTAIL_STYLE_COL.value,
+            MenuColNames.INGREDIENT_OF_COCKTAIL_FAMILY_COL.value,
+            # MenuColNames.MENU_ITEM_NAME_COL.value, 
+            # MenuColNames.MENU_ITEM_TYPE_COL.value
+        ]
         # todo: sales uplift
-        self.cross_metrics = ["sales_uplift"]
+        self.sales_uplift_metrics = [MenuColNames.SALES_UPLIFT_METRIC.value]
         # joining dimensions (lowest common granularity) between menu and depletions data
-        self.common_dims = ["venue_id", "product_id"]
-        self.menu_metrics = ["menu_placements", "venue_placements"]
-        self.depletions_metrics = ["sold_9le", "sold_cases"]
+        self.common_dims = [MenuColNames.VENUE_ID_COL.value, MenuColNames.PRODUCT_ID_COL.value]
+        self.menu_metrics = [MenuColNames.MENU_PLACEMENTS_METRIC.value, MenuColNames.VENUE_PLACEMENTS_METRIC.value]
+        self.depletions_metrics = [MenuColNames.SOLD_9LE_METRIC.value, MenuColNames.SOLD_CASES_METRIC.value]
         self.psudo_join_col = "join_col"
-        self.depletions_agg_dict = {"sold_9le": np.sum, "sold_cases": np.sum}
-
+        self.depletions_agg_dict = {MenuColNames.SOLD_9LE_METRIC.value: np.sum, MenuColNames.SOLD_CASES_METRIC.value: np.sum}
+        self.max_time_dimensions = [MenuColNames.MAX_TIME_MONTH_COL.value, MenuColNames.MAX_TIME_QUARTER_COL.value, MenuColNames.MAX_TIME_YEAR_COL.value]
+        
     def pull_data(self,
                   metrics,
                   breakouts=None,
@@ -35,7 +51,7 @@ class DataProvider(object):
 
         menu_metrics = [m for m in metrics if m.get("name") in self.menu_metrics]
         depletion_metrics = [m for m in metrics if m.get("name") in self.depletions_metrics]
-        cocktail_metrics = [m for m in metrics if m.get("name") in self.cross_metrics]
+        sales_uplift_metrics = [m for m in metrics if m.get("name") in self.sales_uplift_metrics]
 
         if filters:
             cocktail_filter_dims = [f["col"] for f in filters if f["col"] in self.cross_dims]
@@ -47,7 +63,9 @@ class DataProvider(object):
         else:
             cocktail_dims = []
 
-        is_cross_query = cocktail_metrics or (depletion_metrics and (cocktail_dims or cocktail_filter_dims))
+        time_period_dims = [d for d in breakouts if d in self.max_time_dimensions]
+
+        is_cross_query = sales_uplift_metrics or (depletion_metrics and (cocktail_dims or cocktail_filter_dims))
 
         dfs = []
         if menu_metrics:
@@ -69,36 +87,100 @@ class DataProvider(object):
             dfs.append(depletion_df)
 
         if is_cross_query:
-            joining_dims = cocktail_dims + self.common_dims
+
+            qualifier_joining_dims = cocktail_dims
+            if sales_uplift_metrics:
+                qualifier_joining_dims = qualifier_joining_dims + time_period_dims + [MenuColNames.VENUE_ID_COL.value]
+            if (depletion_metrics and (cocktail_dims or cocktail_filter_dims)):
+                qualifier_joining_dims = qualifier_joining_dims + [dim for dim in self.common_dims if dim not in qualifier_joining_dims]
 
             # limiting the data to only venues and product relevant for cocktails
             qualifier_df = pull_data(metrics=[],
-                                     breakouts=joining_dims,
+                                     breakouts=qualifier_joining_dims,
                                      filters=filters,
+                                     query_row_limit=10000000, # hardcoded, can get pretty large
                                      dataset_id=self.menu_dataset)
 
             qualifier_df = qualifier_df.drop_duplicates()
 
-            # remove cocktail dims since it's not available on depletion data
-            depl_dims = [b for b in breakouts if b not in cocktail_dims]
-            depl_fils = [f for f in filters if f["col"] not in self.cross_dims]
-            pre_depletion_df = pull_data(metrics=depletion_metrics,
-                                         breakouts=depl_dims + self.common_dims,
-                                         filters=depl_fils,
-                                         order_cols=order_cols,
-                                         query_row_limit=query_row_limit,
-                                         dataset_id=self.depletion_dataset)
+            if sales_uplift_metrics:
 
-            # this join should add all the dims as breakouts with cocktails dim coming from qualifier_df
-            # this might do intended cross join for product used in multiple cocktails
-            depletion_df = pd.merge(pre_depletion_df, qualifier_df, on=self.common_dims, how='inner')
-            depl_mets = [m.get("name") for m in depletion_metrics]
-            agg_dict = {k: v for k, v in self.depletions_agg_dict.items() if k in depl_mets}
-            if breakouts:
-                depletion_df = depletion_df.groupby(breakouts).agg(agg_dict).reset_index()
-            else:
-                depletion_df = depletion_df.agg(agg_dict).to_frame().T
-            dfs.append(depletion_df)
+                uplift_common_dims = [MenuColNames.VENUE_ID_COL.value] + time_period_dims
+
+                # only concerned with venues for sales uplift
+                qualifier_df = qualifier_df[cocktail_dims + uplift_common_dims].drop_duplicates()
+
+                # remove cocktail dims since it's not available on depletion data
+                depl_dims = [b for b in breakouts if b not in cocktail_dims and b not in uplift_common_dims]
+                depl_fils = [f for f in filters if f["col"] not in self.cross_dims] if filters else []
+                
+                sales_uplift_df = pull_data(
+                    metrics=sales_uplift_metrics,
+                    breakouts=depl_dims + uplift_common_dims,
+                    filters=depl_fils,
+                    order_cols=order_cols,
+                    dataset_id=self.depletion_dataset
+                )
+
+                # get the total average sales uplift across the time period breakout
+                # time_period_dims = [d for d in depl_dims if d in self.max_time_dimensions]
+                sales_uplift_total_avg_agg_dict = {sales_uplift_metrics[0].get("name"): np.mean}
+                if time_period_dims:
+                    total_avg_sales_uplift_df = sales_uplift_df.groupby(time_period_dims).agg(sales_uplift_total_avg_agg_dict).reset_index()
+                else:
+                    total_avg_sales_uplift_df = sales_uplift_df.agg(sales_uplift_total_avg_agg_dict).to_frame().T
+                total_avg_sales_uplift_df = total_avg_sales_uplift_df.rename(columns={sales_uplift_metrics[0].get("name"): "total_avg"})
+
+                # join the sales uplift df with the qualifier df on the common dims
+                sales_uplift_df = pd.merge(sales_uplift_df, qualifier_df, on=uplift_common_dims, how='inner')
+
+                # join the sales uplift df with the total avg sales uplift df on the time period dims, aggregate by time period dims
+                if time_period_dims:
+                    sales_uplift_df = pd.merge(sales_uplift_df, total_avg_sales_uplift_df, on=time_period_dims, how='left')
+                else:
+                    # total_avg_sales_uplift_df should be a scalar
+                    sales_uplift_df["total_avg"] = total_avg_sales_uplift_df.iloc[0]["total_avg"]
+
+                sales_uplift_joined_agg_dict = {sales_uplift_metrics[0].get("name"): np.mean, "total_avg": np.sum}
+                agg_dims = cocktail_dims + time_period_dims
+                if "date_column" in sales_uplift_df.columns and time_period_dims: 
+                    # keep the trend date_column, which will mirror the time_period dim used
+                    agg_dims.append("date_column")
+
+                if agg_dims:
+                    sales_uplift_df = sales_uplift_df.groupby(agg_dims).agg(sales_uplift_joined_agg_dict).reset_index()
+                else:
+                    sales_uplift_df = sales_uplift_df.agg(sales_uplift_joined_agg_dict).to_frame().T
+                
+                # calculate the sales uplift
+                sales_uplift_df[MenuColNames.SALES_UPLIFT_METRIC.value] = sales_uplift_df[MenuColNames.SALES_UPLIFT_METRIC.value] / sales_uplift_df["total_avg"]
+
+                # drop the total_avg column
+                sales_uplift_df = sales_uplift_df.drop(columns=["total_avg"])
+                dfs.append(sales_uplift_df)
+
+            if (depletion_metrics and (cocktail_dims or cocktail_filter_dims)):
+
+                # remove cocktail dims since it's not available on depletion data
+                depl_dims = [b for b in breakouts if b not in cocktail_dims]
+                depl_fils = [f for f in filters if f["col"] not in self.cross_dims] if filters else []
+                pre_depletion_df = pull_data(metrics=depletion_metrics,
+                                            breakouts=depl_dims + self.common_dims,
+                                            filters=depl_fils,
+                                            order_cols=order_cols,
+                                            query_row_limit=query_row_limit,
+                                            dataset_id=self.depletion_dataset)
+
+                # this join should add all the dims as breakouts with cocktails dim coming from qualifier_df
+                # this might do intended cross join for product used in multiple cocktails
+                depletion_df = pd.merge(pre_depletion_df, qualifier_df, on=self.common_dims, how='inner')
+                depl_mets = [m.get("name") for m in depletion_metrics]
+                agg_dict = {k: v for k, v in self.depletions_agg_dict.items() if k in depl_mets}
+                if breakouts:
+                    depletion_df = depletion_df.groupby(breakouts).agg(agg_dict).reset_index()
+                else:
+                    depletion_df = depletion_df.agg(agg_dict).to_frame().T
+                dfs.append(depletion_df)
 
         if len(dfs) > 1:
             df = dfs[0]
